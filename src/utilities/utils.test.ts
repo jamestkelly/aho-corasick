5a0faa69751307abb6a1e0dc68dd7ac0fe9f5501
jamestkelly <jimkelly.t@outlook.com>
2023-12-10 16:10:14 +1000

    feat: added alphanumeric string check

--- src/utilities/utils.test.ts
+++ src/utilities/utils.test.ts
@@ -1,10 +1,10 @@
-import { isNonEmptyArray } from './utils';
+import { isAlphanumeric, isNonEmptyArray } from './utils';
 
 /**
  * Test suite for the Utils functions.
  */
 describe('utils', () => {
-  // Test for valid arrays
+  // Test for valid arrays with `isNonEmptyArray`
   describe('isNonEmptyArray', () => {
     // Test case for valid array (populated)
     test('with valid arrays', () => {
@@ -25,6 +25,28 @@ describe('utils', () => {
       let arrayA: Array<string> | undefined;
 
       expect(isNonEmptyArray(arrayA)).toBeFalsy();
+    });
+  });
+
+  // Test for alphanumeric strings with `isAlphanumeric`
+  describe('isAlphanumeric', () => {
+    // Test case for valid strings
+    test('with valida alphanumeric strings', () => {
+      expect(isAlphanumeric('hello')).toBeTruthy();
+      expect(isAlphanumeric('world')).toBeTruthy();
+      expect(isAlphanumeric('123456')).toBeTruthy();
+      expect(isAlphanumeric('abcdef')).toBeTruthy();
+      expect(isAlphanumeric('ABCDEF')).toBeTruthy();
+      expect(isAlphanumeric('h3ll0')).toBeTruthy();
+      expect(isAlphanumeric('w0r1d')).toBeTruthy();
+    });
+
+    // Test case for invalid strings
+    test('with invalid alphanumeric string', () => {
+      expect(isAlphanumeric('!!')).toBeFalsy();
+      expect(isAlphanumeric('word1 word2')).toBeFalsy();
+      expect(isAlphanumeric('$$$$')).toBeFalsy();
+      expect(isAlphanumeric('')).toBeFalsy();
     });
   });
 });
--- src/utilities/utils.ts
+++ src/utilities/utils.ts
@@ -9,3 +9,21 @@ export function isNonEmptyArray(arr: any): boolean {
   return Array.isArray(arr) && arr.length > 0;
 }
 
+/**
+ * @description
+ * Checks if a given string contains only alphanumeric characters.
+ *
+ * Alphanumeric characters include uppercase and lowercase letters (A-Z, a-z)
+ * and numeric digits (0-9).
+ *
+ * @param {string} str - The input string to be checked.
+ * @returns {boolean} Returns true if the string is alphanumeric, false otherwise.
+ *
+ * @example
+ * isAlphaNumeric("abc123"); // true
+ * isAlphaNumeric("hello_world"); // false (contains underscore)
+ * isAlphaNumeric("123$%^"); // false (contains special characters)
+ */
+export function isAlphanumeric(str: string): boolean {
+  return /^[a-zA-Z0-9]+$/.test(str);
+}
