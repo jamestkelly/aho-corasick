62f4323f704b5e205636b77e4d1daf6609d078b6
jamestkelly <jimkelly.t@outlook.com>
2023-12-13 22:28:37 +1000

    bugfix: added tslint disable unused on exception throwing tests

--- src/trie.test.ts
+++ src/trie.test.ts
@@ -20,6 +20,7 @@ describe('Trie', () => {
 
     test('keywords array of empty strings to throw an error', () => {
       expect(() => {
+        /* tslint:disable:no-unused-variable */
         const _trie = new Trie(['', '']);
       }).toThrow('Provided string `keyword` is a null or empty string.');
     });
